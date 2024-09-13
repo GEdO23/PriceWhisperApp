97c7be709762e07774587732d09ca275650505ce
Gabriel Eringer <rm99632@fiap.com.br>
2024-09-13 17:51:03 -0300

    Carregando produtos vindos do Realtime Database do Firebase

--- app/src/main/java/br/com/pricewhisper/ui/activity/ProductsListActivity.kt
+++ app/src/main/java/br/com/pricewhisper/ui/activity/ProductsListActivity.kt
@@ -2,31 +2,80 @@ package br.com.pricewhisper.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import br.com.pricewhisper.R
 import br.com.pricewhisper.models.Product
 import br.com.pricewhisper.ui.recycler.adapter.ProductsListAdapter
-import java.math.BigDecimal
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import okhttp3.Call
+import okhttp3.Callback
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import okhttp3.Response
+import java.io.IOException
 
 class ProductsListActivity : AppCompatActivity() {
+
+    private val httpClient = OkHttpClient()
+    private val gson = Gson()
+    private val url = "https://pricewhisper-auth-cc2c8-default-rtdb.firebaseio.com/products.json"
+    private val productsList: MutableList<Product> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_products_list)
         setTitle("Lista de Produtos")
 
+        /* Hard coded ProductsList
         val productsList: MutableList<Product> = arrayListOf(
             Product("Arroz 5kg", BigDecimal(47.95)),
             Product("Feijão 5kg", BigDecimal(42.00))
-        )
-
+        )*/
+        
         val productsListRecycler: RecyclerView = findViewById(R.id.products_list_recycler)
         productsListRecycler.layoutManager = LinearLayoutManager(this)
-        productsListRecycler.adapter = ProductsListAdapter(this, productsList) { position ->
+        val adapter = ProductsListAdapter(this, productsList) { i ->
             val intent = Intent(this@ProductsListActivity, ProductDetailsActivity::class.java)
-            intent.putExtra("product", productsList[position])
+            intent.putExtra("product", productsList[i])
             startActivity(intent)
         }
+        productsListRecycler.adapter = adapter
+
+        val requestGetMethod = Request.Builder()
+            .url(url)
+            .get()
+            .build()
+
+        val responseGetMethod = object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("LoadFirebase", e.message.toString())
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val localBody = response.body
+                val productsJson = localBody?.string()
+
+                val productsMapType = object : TypeToken<Map<String, Product>>() {}.type
+                val productsMap: Map<String, Product> = gson.fromJson(productsJson, productsMapType)
+
+                val products = productsMap.values.toList()
+
+                runOnUiThread {
+                    products.forEach {
+                        productsList.add(it)
+                        Log.d("Produto", it.toJson())
+                    }
+                    adapter.notifyDataSetChanged()
+                }
+            }
+
+        }
+
+        httpClient.newCall(requestGetMethod)
+            .enqueue(responseGetMethod)
     }
 }
