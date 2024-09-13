14fabeaad82300cb42be5da78082b9e51fab7a71
GEdO23 <rm99632@fiap.com.br>
2024-09-13 09:23:10 -0300

    Adcionando tela de Lista de Produtos com RecyclerView, item produto da RecyclerView e navegação entre a tela Home e a tela de Lista de Produtos

--- app/src/main/java/com/example/pricewhisper/HomeActivity.kt
+++ app/src/main/java/com/example/pricewhisper/HomeActivity.kt
@@ -1,6 +1,8 @@
 package com.example.pricewhisper
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,5 +13,11 @@ class HomeActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
         setTitle("Home")
+
+        val btnProductsList: TextView = findViewById(R.id.home_btn_products_list)
+        btnProductsList.setOnClickListener {
+            val intent = Intent(this@HomeActivity, ProductsListActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
