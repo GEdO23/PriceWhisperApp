d50d3b6ffb8e19867badd54881f480d98715ff4d
Gabriel Eringer <rm99632@fiap.com.br>
2024-09-13 18:49:16 -0300

    Mostrando icone para salvar produto na tela de cadastro de produto

--- app/src/main/java/br/com/pricewhisper/ui/activity/RegisterProductActivity.kt
+++ app/src/main/java/br/com/pricewhisper/ui/activity/RegisterProductActivity.kt
@@ -1,13 +1,21 @@
 package br.com.pricewhisper.ui.activity
 
 import android.os.Bundle
+import android.view.Menu
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import br.com.pricewhisper.R
+import br.com.pricewhisper.models.Product
 
 class RegisterProductActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register_product)
         setTitle("Cadastro de Produto")
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_save, menu)
+        return super.onCreateOptionsMenu(menu)
     }
 }
