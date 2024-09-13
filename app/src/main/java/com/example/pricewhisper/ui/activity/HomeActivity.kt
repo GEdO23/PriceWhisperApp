3997d1186668bd4bc52c7c109c83632d95fac134
GEdO23 <rm99632@fiap.com.br>
2024-09-13 10:12:31 -0300

    Movendo activitys para diretório ui.activity

--- app/src/main/java/com/example/pricewhisper/ui/activity/HomeActivity.kt
+++ app/src/main/java/com/example/pricewhisper/ui/activity/HomeActivity.kt
@@ -1,12 +1,10 @@
-package com.example.pricewhisper
+package com.example.pricewhisper.ui.activity
 
 import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.pricewhisper.R
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/pricewhisper/ui/activity/ProductsListActivity.kt
+++ app/src/main/java/com/example/pricewhisper/ui/activity/ProductsListActivity.kt
@@ -1,10 +1,8 @@
-package com.example.pricewhisper
+package com.example.pricewhisper.ui.activity
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.pricewhisper.R
 
 class ProductsListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
