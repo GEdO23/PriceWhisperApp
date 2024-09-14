f7b19030d9bab6594b2e14176977cdede180ed91
Gabriel Eringer <rm99632@fiap.com.br>
2024-09-13 21:44:03 -0300

    Tornando Locale("PT", "BR") uma constante em Constants.kt

--- app/src/main/java/br/com/pricewhisper/utils/Constants.kt
+++ app/src/main/java/br/com/pricewhisper/utils/Constants.kt
@@ -1,7 +1,9 @@
 package br.com.pricewhisper.utils
 
 import okhttp3.OkHttpClient
+import java.util.Locale
 
 const val RTDB_PRODUCTS_URL = "https://pricewhisper-auth-cc2c8-default-rtdb.firebaseio.com/products.json"
 const val PRODUCT_KEY = "product"
 val httpClient = OkHttpClient()
+val appLocale = Locale("PT", "BR")
--- app/src/main/java/br/com/pricewhisper/utils/CurrencyUtil.kt
+++ app/src/main/java/br/com/pricewhisper/utils/CurrencyUtil.kt
@@ -2,10 +2,9 @@ package br.com.pricewhisper.utils
 
 import android.icu.text.DecimalFormat
 import java.math.BigDecimal
-import java.util.Locale
 
 class CurrencyUtil {
-    private val priceFormat = DecimalFormat.getCurrencyInstance(Locale("PT", "BR"))
+    private val priceFormat = DecimalFormat.getCurrencyInstance(appLocale)
 
     fun format(price: BigDecimal): String = priceFormat.format(price)
 }
