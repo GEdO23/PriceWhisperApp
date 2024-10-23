5583f8ba4bb919b526f720bb5675e48202862463
GEdO23 <rm99632@fiap.com.br>
2024-10-23 15:23:18 -0300

    Criando composables OutlineGroupButtons e GroupButton

--- app/src/main/java/br/com/pricewhisper/MainActivity.kt
+++ app/src/main/java/br/com/pricewhisper/MainActivity.kt
@@ -5,19 +5,32 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.widthIn
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.FilledIconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import br.com.pricewhisper.models.Product
 import br.com.pricewhisper.ui.theme.PriceWhisperTheme
 import com.google.gson.Gson
@@ -47,10 +60,41 @@ class MainActivity : ComponentActivity() {
 fun HomeScreen() {
     PriceWhisperTheme {
         Surface(Modifier.fillMaxSize()) {
-            Column {
-                FormNewProduct()
+            Column(Modifier.padding(16.dp)) {
+//                FormNewProduct()
+                OutlineGroupButtons()
             }
         }
+    }
+}
+
+@Composable
+fun OutlineGroupButtons() {
+    Row(
+        Modifier
+            .fillMaxWidth()
+            .border(2.dp, Color.Gray, RoundedCornerShape(24.dp))
+            .padding(32.dp)
+    ) {
+        GroupButton(label = "Products") {}
+        GroupButton(label = "Profile") {}
+        GroupButton(label = "Settings") {}
+    }
+}
+
+
+@Composable
+fun GroupButton(
+    label: String,
+    onClick: () -> Unit
+) {
+    Column(
+        Modifier.widthIn(min = 100.dp, max = 100.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(4.dp)
+    ) {
+        FilledIconButton(onClick = onClick) { }
+        Text(text = label, textAlign = TextAlign.Center)
     }
 }
 
