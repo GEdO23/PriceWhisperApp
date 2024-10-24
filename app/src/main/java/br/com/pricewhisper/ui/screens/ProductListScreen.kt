d8150359fbccf45bd139cdac4e6a3d2d5d01b912
GEdO23 <rm99632@fiap.com.br>
2024-10-24 11:31:46 -0300

    Adcionando navegação para a tela de formulario de produto

--- app/src/main/java/br/com/pricewhisper/ui/screens/ProductListScreen.kt
+++ app/src/main/java/br/com/pricewhisper/ui/screens/ProductListScreen.kt
@@ -1,15 +1,25 @@
 package br.com.pricewhisper.ui.screens
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
+import br.com.pricewhisper.productFormDestination
 import br.com.pricewhisper.ui.viewmodels.ProductViewModel
 
 @Composable
@@ -19,16 +29,29 @@ fun ProductListScreen(
 ) {
     val viewModel = ProductViewModel()
 
-    Column(modifier = modifier) {
-        Text(
-            text = "Products List",
-            fontSize = 24.sp,
-            fontWeight = FontWeight(700)
-        )
+    Box {
+        Column(modifier = modifier) {
+            Text(
+                text = "Products List",
+                fontSize = 24.sp,
+                fontWeight = FontWeight(700)
+            )
 
-        viewModel.getAll()
+            ProductList(viewModel = viewModel)
+        }
 
-        ProductList(viewModel = viewModel)
+        FloatingActionButton(
+            onClick = { navController.navigate(productFormDestination.route) },
+            containerColor = MaterialTheme.colorScheme.primary,
+            modifier = Modifier
+                .align(alignment = Alignment.BottomEnd)
+                .padding(16.dp)
+        ) {
+            Icon(
+                imageVector = Icons.Filled.Add,
+                contentDescription = "Add new product"
+            )
+        }
     }
 }
 
@@ -37,11 +60,13 @@ private fun ProductList(
     viewModel: ProductViewModel,
     modifier: Modifier = Modifier
 ) {
+    viewModel.getAll()
+
     LazyColumn(modifier = modifier) {
         items(items = viewModel.productList) {
             ListItem(
                 headlineContent = { Text(it.name) },
-                supportingContent = { Text("R$${it.price}") }
+                supportingContent = { Text("R$ ${it.price}") }
             )
         }
     }
