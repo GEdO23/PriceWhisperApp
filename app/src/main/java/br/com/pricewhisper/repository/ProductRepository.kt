ede0b31d38a377d1aac6160eeadf0de0dee31394
GEdO23 <rm99632@fiap.com.br>
2024-10-24 10:14:37 -0300

    Coletando produtos do Firebase

--- app/src/main/java/br/com/pricewhisper/repository/ProductRepository.kt
+++ app/src/main/java/br/com/pricewhisper/repository/ProductRepository.kt
@@ -19,6 +19,39 @@ class ProductRepository {
     private val gson = Gson()
     private val url: String = "https://omcorp-pricewhisper-default-rtdb.firebaseio.com"
 
+    fun loadFirebase(productList: SnapshotStateList<Product>) {
+        val request = Request.Builder()
+            .url("$url/products.json")
+            .get()
+            .build()
+
+        val response = object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                Log.e("PRICE_WHISPER", e.message.toString())
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                val localBody = response.body?.string() ?: ""
+                Log.d("PRICE_WHISPER", localBody)
+                if (localBody != "" && localBody != "null") {
+                    val typeToken = object : TypeToken<HashMap<String?, Product?>>() {}.type
+                    val mapProduct: HashMap<String?, Product?> =
+                        gson.fromJson(localBody, typeToken)
+
+                    productList.clear()
+                    mapProduct.forEach { (id, product) ->
+                        if (product != null) {
+                            productList.add(product)
+                        }
+                    }
+                }
+            }
+        }
+
+        httpClient.newCall(request)
+            .enqueue(response)
+    }
+
     fun saveInFirebase(product: Product) {
         val json = gson.toJson(product)
         val body = json.toRequestBody("application/json".toMediaType())
