335d124e9fd8a385044b76c5030fd9860b136fb0
GEdO23 <rm99632@fiap.com.br>
2024-10-23 22:54:23 -0300

    Movendo Screens e ViewModel para suas pastas

--- app/src/main/java/br/com/pricewhisper/ui/screens/PriceWhisperScreen.kt
+++ app/src/main/java/br/com/pricewhisper/ui/screens/PriceWhisperScreen.kt
@@ -1,4 +1,4 @@
-package br.com.pricewhisper
+package br.com.pricewhisper.ui.screens
 
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -7,7 +7,6 @@ import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import br.com.pricewhisper.ui.ProductFormScreen
 import br.com.pricewhisper.ui.theme.PriceWhisperTheme
 
 @Composable
