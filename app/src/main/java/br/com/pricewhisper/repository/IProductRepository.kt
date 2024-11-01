9d7ec3413b071a007892e1eca02ca17ec5051866
GEdO23 <rm99632@fiap.com.br>
2024-11-01 16:01:17 -0300

    Criando métodos onRequestSuccess e onRequestFailure para cada método de ProductRepository
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/pricewhisper/repository/IProductRepository.kt
+++ app/src/main/java/br/com/pricewhisper/repository/IProductRepository.kt
@@ -1,53 +1,71 @@
 package br.com.pricewhisper.repository
 
-import androidx.compose.runtime.snapshots.SnapshotStateList
 import br.com.pricewhisper.models.Product
+import java.io.IOException
 
 interface IProductRepository {
     /**
      * Collects the [Product] List from the Firebase Realtime Database
-     * @param productList The [Product] List that will be replaced by the new [Product] List
+     * @param onRequestSuccess What should happen if the request succeeds
+     * @param onRequestFailure What should happen if the request fails
      * @see Product
      */
     fun getProductListFromFirebase(
-        productList: SnapshotStateList<Product>
+        onRequestSuccess: (products: HashMap<String, Product?>?) -> Unit,
+        onRequestFailure: (e: IOException) -> Unit
     )
 
     /**
      * Gets a [Product] from the Firebase Realtime Database [Product] List by its ID
      * @param id The identifier of the [Product] to be found
+     * @param onRequestSuccess What should happen if the request succeeds
+     * @param onRequestFailure What should happen if the request fails
      * @see Product
      */
     fun getProductFromFirebaseById(
-        id: String
+        id: String,
+        onRequestSuccess: (productFound: Product?) -> Unit,
+        onRequestFailure: (e: IOException) -> Unit
     )
 
     /**
      * Saves a new [Product] on the Firebase Realtime Database [Product] List
      * @param product The [Product] to be saved
+     * @param onRequestSuccess What should happen if the request succeeds
+     * @param onRequestFailure What should happen if the request fails
      * @see Product
      */
     fun postProductToFirebase(
-        product: Product
+        product: Product,
+        onRequestSuccess: (productSaved: Product) -> Unit,
+        onRequestFailure: (e: IOException) -> Unit
     )
 
     /**
      * Updates a [Product] in the Firebase Realtime Database [Product] List
      * @param id The identifier of the [Product] to be replaced
      * @param newProduct The new [Product] to replace the old [Product]
+     * @param onRequestSuccess What should happen if the request succeeds
+     * @param onRequestFailure What should happen if the request fails
      * @see Product
      */
     fun updateProductInFirebase(
         id: String,
-        newProduct: Product
+        newProduct: Product,
+        onRequestSuccess: (oldProduct: Product, newProduct: Product) -> Unit,
+        onRequestFailure: (e: IOException) -> Unit
     )
 
     /**
      * Deletes a [Product] in the Firebase Realtime Database [Product] List
      * @param id The identifier of the [Product] to be deleted
+     * @param onRequestSuccess What should happen if the request succeeds
+     * @param onRequestFailure What should happen if the request fails
      * @see Product
      */
     fun deleteProductInFirebase(
-        id: String
+        id: String,
+        onRequestSuccess: (productDeleted: Product) -> Unit,
+        onRequestFailure: (e: IOException) -> Unit
     )
 }
