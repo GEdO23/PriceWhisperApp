536bffdce43b800d20d1e45e290837dd73862203
GEdO23 <rm99632@fiap.com.br>
2024-11-04 15:58:11 -0300

    Documentar Product model
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/pricewhisper/models/Product.kt
+++ app/src/main/java/br/com/pricewhisper/models/Product.kt
@@ -2,6 +2,15 @@ package br.com.pricewhisper.models
 
 import java.math.BigDecimal
 
+/**
+ * Data class representing a Product.
+ *
+ * @property id The unique identifier for the product.
+ * @property name The name of the product.
+ * @property price The price of the product.
+ * @property stock The stock quantity of the product.
+ * @property description A brief description of the product.
+ */
 data class Product(
     var id: String = "",
     var name: String = "",
