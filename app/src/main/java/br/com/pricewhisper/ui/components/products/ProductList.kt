0f4076da1e2c4fc8be0469f3f047090609860026
GEdO23 <rm99632@fiap.com.br>
2024-11-04 10:24:51 -0300

    #37 Colocando botao para deletar produto no item de cada produto
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/pricewhisper/ui/components/products/ProductList.kt
+++ app/src/main/java/br/com/pricewhisper/ui/components/products/ProductList.kt
@@ -4,23 +4,32 @@ import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Delete
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.ListItem
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import br.com.pricewhisper.models.Product
+import java.math.BigDecimal
 
 @Composable
 fun ProductList(
     modifier: Modifier = Modifier,
     productList: List<Product>,
-    onClickItem: (productClicked: Product) -> Unit
+    onClickItem: (productClicked: Product) -> Unit,
+    onClickDelete: (id: String) -> Unit
 ) {
     LazyColumn(modifier = modifier) {
         items(items = productList) { product ->
             ProductListItem(
                 product = product,
-                onClick = onClickItem
+                onClick = onClickItem,
+                onClickDelete = onClickDelete
             )
         }
     }
@@ -30,12 +39,46 @@ fun ProductList(
 private fun ProductListItem(
     modifier: Modifier = Modifier,
     product: Product,
-    onClick: (productClicked: Product) -> Unit
+    onClick: (productClicked: Product) -> Unit,
+    onClickDelete: (id: String) -> Unit
 ) {
     Box(modifier = modifier.clickable { onClick(product) }) {
         ListItem(
-            headlineContent = { Text(product.name) },
-            supportingContent = { Text("R$ ${product.price}") }
+            headlineContent = {
+                Text(product.name)
+            },
+            supportingContent = {
+                Text("R$ ${product.price}")
+            },
+            trailingContent = {
+                IconButton(
+                    onClick = { onClickDelete(product.id) }
+                ) {
+                    Icon(
+                        imageVector = Icons.Outlined.Delete,
+                        tint = MaterialTheme.colorScheme.onSurface,
+                        contentDescription = "Delete Product"
+                    )
+
+                }
+            }
         )
     }
+}
+
+@Preview
+@Composable
+private fun ProductListItemPreview() {
+    val productExample = Product(
+        name = "Produto Exemplo",
+        price = BigDecimal("23.00"),
+        stock = 10u,
+        description = "Produto exemplo a fim de testes"
+    )
+
+    ProductListItem(
+        product = productExample,
+        onClick = {},
+        onClickDelete = {}
+    )
 }
