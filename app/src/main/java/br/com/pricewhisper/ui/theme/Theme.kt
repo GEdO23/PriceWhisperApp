363f3e2fcd523cfb35bc4310e2a9356ef825e9dc
GEdO23 <rm99632@fiap.com.br>
2024-11-04 15:53:01 -0300

    Implementar Dark mode #18
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/pricewhisper/ui/theme/Theme.kt
+++ app/src/main/java/br/com/pricewhisper/ui/theme/Theme.kt
@@ -1,14 +1,16 @@
 package br.com.pricewhisper.ui.theme
 
-import android.os.Build
-import androidx.compose.foundation.isSystemInDarkTheme
+import android.app.Activity
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.platform.LocalContext
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowCompat
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
@@ -34,19 +36,21 @@ private val LightColorScheme = lightColorScheme(
 
 @Composable
 fun PriceWhisperTheme(
-    darkTheme: Boolean = isSystemInDarkTheme(),
-    // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    darkMode: MutableState<Boolean> = mutableStateOf(false),
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
-        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
-        }
-
-        darkTheme -> DarkColorScheme
+        darkMode.value -> DarkColorScheme
         else -> LightColorScheme
+    }
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        SideEffect {
+            val window = (view.context as Activity).window
+            window.statusBarColor = colorScheme.primary.toArgb()
+            WindowCompat.getInsetsController(window, view)
+                .isAppearanceLightStatusBars = !darkMode.value
+        }
     }
 
     MaterialTheme(
