323c9af1f3241474af8cde68f2194c5b4a51aed4
GEdO23 <rm99632@fiap.com.br>
2024-11-04 16:55:27 -0300

    Documentar ThemeViewModel e seus metodos
    Signed-off-by: GEdO23 <rm99632@fiap.com.br>

--- app/src/main/java/br/com/pricewhisper/ui/viewmodels/ThemeViewModel.kt
+++ app/src/main/java/br/com/pricewhisper/ui/viewmodels/ThemeViewModel.kt
@@ -7,6 +7,9 @@ import androidx.lifecycle.ViewModel
 
 private const val SHARED_PREFS_PALETTE_KEY = "IS_DARK_MODE_ON"
 
+/**
+ * ViewModel class for managing theme-related operations.
+ */
 class ThemeViewModel : ViewModel() {
     val currentPrefs: MutableState<SharedPreferences?> = mutableStateOf(null)
     val isDarkModeOn: MutableState<Boolean> = mutableStateOf(false)
@@ -16,12 +19,20 @@ class ThemeViewModel : ViewModel() {
         isDarkModeOn.value = false
     }
 
+    /**
+     * Retrieves the current palette mode from SharedPreferences.
+     *
+     * @return Boolean indicating whether dark mode is enabled.
+     */
     fun getCurrentPalette(): Boolean {
         return currentPrefs.value
             ?.getBoolean(SHARED_PREFS_PALETTE_KEY, false)
             ?: false
     }
 
+    /**
+     * Switches the palette mode and updates SharedPreferences.
+     */
     fun switchPalette() {
         currentPrefs.value
             ?.edit()
